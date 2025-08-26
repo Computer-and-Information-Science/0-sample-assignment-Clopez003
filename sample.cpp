6c05edcf23ae916fc86a6c16cc62b5511955562a
Clopez003 <christlopez6016@gmail.com>
2025-08-26 17:55:16 +0000

    completed

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,12 @@
-// YOUR NAME HERE
+// Chris Lopez
 #include <iostream>
 using namespace std;
 
 int main () {
-
+int a,b;
+cout<<"Enter first integer: ";
+cin>>a;
+cout<<"Enter second integer: ";
+cin>>b;
+cout<< endl <<"The sum is " << a+b << ".\n";
 }
